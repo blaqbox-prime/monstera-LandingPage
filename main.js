c8e98b296e3df7ff16894648dc34f913d03b7479
Karabo Sambo <58365025+blaqbox-prime@users.noreply.github.com>
2021-01-04 11:44:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const searchbox = document.getElementById('searchbox');
+
+function openSearch(){
+    searchbox.classList.toggle('.search-hidden');
+    searchbox.classList.toggle('search-open');
+}
